cf5529fb5cdc2b206a6339e8963ae901f6e0d289
Robert Sibek <robert.sibek@gmail.com>
2018-03-16 16:42:47 +0800

    Initial commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+Website template for future projects.
+
+Created using Vue, Bootstrap, SASS, JS.
+
+Free to user, so enjoy.
---
+++ scripts/base.js
@@ -0,0 +1,5 @@
+function greetings() {
+	var msg = "Today " + Date() + " is the best day of your life!";
+	alert(msg);
+	console.log(msg);
+}
